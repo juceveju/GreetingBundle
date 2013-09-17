d87d8981e751f33f6fbaad55bd6214012e1eb715
juceveju <jc.velasco.juarez@gmail.com>
2013-09-17 15:52:35 +0200

    testing git

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+	// only one comment
     public function indexAction($name)
     {
         return $this->render('JucevejuGreetingBundle:Default:index.html.twig', array('name' => $name));
